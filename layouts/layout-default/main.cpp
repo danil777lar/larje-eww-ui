e44bbd6e58cd107893ee1fb98a8db1f9b87f0966
danchik1337 <danil777lar@gmail.com>
2025-07-24 18:23:35 +0400

    lasknldnka

--- layouts/layout-default/main.cpp
+++ layouts/layout-default/main.cpp
@@ -5,11 +5,52 @@
 #include <unordered_map>
 #include <cmath>
 
+#include "json.hpp"
 #include "Label.h"
 #include "larje_utils.hpp"
 #include "eww_ui/Box.h"
 
 using string = std::string;
+
+string get_widget_name(string widget_command, int index) {
+
+    string name = "";//config_content["name"];
+    name += "-" + std::to_string(index);
+    name += "-" + widget_command.substr(0, widget_command.find(' '));
+
+    return name;
+}
+
+string get_widgets(nlohmann::json box, int depth, std::vector<string>* widget_out) {
+    string spacing = "";
+    for (int i = 0; i <= depth; ++i) {
+        spacing += "  ";
+    }
+
+    string result = "";
+    result += "(box\n";
+
+    string orientation = "\"" + box.value("orientation", "v") + "\"";
+    result += spacing + ":orientation " + orientation + "\n";
+
+    if (box.contains("widgets")) {
+        for (auto& key : box["widgets"]) {
+            if (key.is_string()) {
+                string widget_name = get_widget_name(key.get<string>(), widget_out->size());
+                string command = key.get<string>() + " --var_name " + "var-" + widget_name;
+                widget_out->push_back(command);
+                result += spacing + "(" + widget_name + ")" + "\n";
+            }
+            else {
+                result += spacing + get_widgets(key, depth + 1, widget_out);
+            }
+        }
+    }
+
+    result += ")";
+
+    return result;
+}
 
 int main(int argc, char **argv) {
     std::unordered_map<std::string, std::string> args = process_args(argc, argv);
